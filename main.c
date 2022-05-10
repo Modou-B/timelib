fc31317a3a3466f8071e07656858a20dba74f2d4
Modou-B <modou.be@gmail.com>
2022-05-10 13:07:43 +0200

    Ádd Date structure

--- main.c
+++ main.c
@@ -1,84 +1,148 @@
-/**
- *
- *
- *
- **/
-
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- *
- *
- **/
-int day_of_the_year(int day, int month, int year)
+
+///////////////////////////////////////////////
+struct curr_date {
+    int day, month, year;
+
+};
+
+///////////////////////////////////////////////
+
+
+
+
+///////////////////////////////////////////////
+// int start_functions(Curr_date){
+
+
+// }
+
+///////////////////////////////////////////////
+struct curr_date input_date()
 {
 
-    return 0;
-}
+struct curr_date c_date1;
 
-
-
-int main()
-{
-    int curr_day;
-    int curr_month;
-    int curr_year;
-    int counter1 = 0;
-    int counter2 = 0;
-    //int current_year_of_programming;
     int tage_pro_monat[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
-
-
-    //current_year_of_programming = 2022;
 
     while (1) {
         printf("Geben Sie das Jahr ein: ");
-        scanf("%i",&curr_year);
+        scanf("%i",&c_date1.year);
 
-        if (curr_year != 0) {
+
+        if (c_date1.year != 0) {
+           // get_days_for_month(c_date1.month,c_date1.year);
+
+            if (is_leapyear(c_date1) == 1) {
+              tage_pro_monat[1] = 29;
+            }
             break;
         }
-        //curr_day = 2020;
-        //if (!((curr_year < current_year_of_programming) || (curr_year > current_year_of_programming))){
-        //  break;
-        //}
     }
-    // irgendwas an der Bedingung darunter ist noch falsch (Geht btw auch nicht w�hrung meine Current_year_of_programming aktiv ist)
-    if (curr_year % 4 == 0 && curr_year % 100 != 0 || curr_year % 400 == 0) {
-        tage_pro_monat[0] = 29;
-    }
+
+    //get_days_for_month(c_date1.month, c_date1.curr_);
 
     while (1) {
         printf("\nGeben Sie den Monat ein: ");
-        scanf("%i",&curr_month);
-        //curr_month = 3;
-        if (!((curr_month < 1) || (curr_month > 12))) {
+        scanf("%i",&c_date1.month);
+
+        if (!((c_date1.month < 1) || (c_date1.month > 12))) {
             break;
         }
-    }
 
+    }
 
     while (1) {
+        int temp_month;
         printf("\nGeben Sie den Tag ein: ");
-        scanf("%i",&curr_day);
-        //curr_day = 15;
-        if (!((curr_day < 1) || (curr_day > tage_pro_monat[curr_month - 1]))) {
+        scanf("%i",&c_date1.day);
+        temp_month = c_date1.month - 1;
+
+        if (!((c_date1.day < 1) || (c_date1.day > get_days_for_month(c_date1)))) {
             break;
         }
+
     }
+    return c_date1;
+}
 
 
 
-    for (int i = 1; i < curr_month; i++) {
+///////////////////////////////////////////////
+int exists_date(struct curr_date c_date1) {
+    if ((c_date1.year < 1582)|| (c_date1.year > 2400))
+    {
+        printf("Date is invalid %i \n",c_date1.year);
+        return 1;
+    }
+    return 0;
+}
+
+
+
+///////////////////////////////////////////////
+int day_of_the_year(struct curr_date c_date1) {
+    int counter1 = 0;
+    int counter2 = 0;
+    int tage_pro_monat[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+     for (int i = 0; i < c_date1.month; i++) {
         counter1 = counter1 + tage_pro_monat[i];
-
-
     }
 
-    counter2 = tage_pro_monat[curr_month] - curr_day;
+    counter2 = tage_pro_monat[c_date1.month] - c_date1.day;
     counter1 = counter1 + counter2;
-    printf("%i",counter1);
+    printf("Day of the year: %i \n",counter1);
+    return 0;
+}
+
+
+
+
+
+///////////////////////////////////////////////
+int is_leapyear(struct curr_date c_date1) {
+    if (c_date1.year% 4 == 0 && c_date1.year % 100 != 0 || c_date1.year % 400 == 0) {
+        return 1;
+    }
+    return 0;
+}
+
+
+
+///////////////////////////////////////////////
+int get_days_for_month(struct curr_date c_date1) {
+    int counter1 = 0;
+    int counter2 = 0;
+    int tage_pro_monat[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+    if (is_leapyear(c_date1) == 1) {
+        tage_pro_monat[1] = 29;
+    }
+
+    return tage_pro_monat[c_date1.month - 1];
+}
+
+int main()
+{
+
+    //start_functions(day, month,year);
+
+    struct curr_date c_date1;
+    c_date1 = input_date();
+
+    printf("%i.%i.%i\n",c_date1.day,c_date1.month,c_date1.year);
+
+    while (exists_date(c_date1) == 1) {
+        c_date1 = input_date();
+    }
+
+    day_of_the_year(c_date1);
+
+    //get_days_for_month(c_date1.month,c_date1.day);
 
     return 0;
 }
+
